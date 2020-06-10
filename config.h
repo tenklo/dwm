f534535ed0400c7b83445915d428391632ca555a
tenklo <tenklo@pm.me>
2020-06-10 17:41:36 +0200

    going to revert

--- config.h
+++ config.h
@@ -51,6 +51,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+    { "(@)",      dwindle },
 };
 
 /* key definitions */
@@ -85,7 +86,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} },
- 	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[4]} },
+ 	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
