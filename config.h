76c5517d48d3e84376d957b7dc75cd8669fc54d6
tenklo <tenklo@pm.me>
2020-06-10 19:11:35 +0200

    testing monitor filter

--- config.h
+++ config.h
@@ -33,10 +33,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	/* class            instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",           NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Thunderbird",    NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "Alacritty",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ "NULL",           NULL,     "matrix",       0,         0,          1,          -1,        1 },
 	{ NULL,             NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
