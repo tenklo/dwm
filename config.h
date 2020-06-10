f5b0ed112220cfb8d85141e1c15510879ad705c7
tenklo <tenklo@pm.me>
2020-06-10 19:19:28 +0200

    reverted tests

--- config.h
+++ config.h
@@ -37,7 +37,6 @@ static const Rule rules[] = {
 	{ "Gimp",           NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Thunderbird",    NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "Alacritty",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
-	{ "NULL",           NULL,     "matrix",       0,         0,          1,          -1,        1 },
 	{ NULL,             NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
