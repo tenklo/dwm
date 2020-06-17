b0c0e7fef195691db8b0ac1033e0565695b68d94
tenklo <tenklo@pm.me>
2020-06-17 22:20:12 +0200

    got floating calculator to work

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16", "Noto Color Emoji:style=Regular:size=16" , "unifont:fontformat=truetype:size=16:antialias=false", "Siji:style=Regular:pixelsize=20"};
+static const char *fonts[]          = { "monospace:size=16", "-misc-font awesome 5 brands regular-medium-r-normal--17-120-100-100-p-0-iso10646-1", "-misc-font awesome 5 free regular-medium-r-normal--17-120-100-100-p-0-iso10646-1", "-misc-font awesome 5 free solid-black-r-normal--17-120-100-100-p-0-iso10646-1"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char focussed[]        = "#0000ff";
 static const char col_gray1[]       = "#222222";
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	{ "whatsapp-nativefier-d40211",      NULL,     NULL,           1 << 7,    0,          0,          -1,        -1 },
 	{ "TelegramDesktop",                 NULL,     NULL,           1 << 7,    0,          0,          -1,        -1 },
 	{ "Alacritty",                       NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ NULL,                            NULL,     "calc",         0,         1,          0,          -1,        -1 },
 	{ NULL,                              NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
