ea17963e01bdb16246060b2f48de06808dfdc8a6
tenklo <tenklo@pm.me>
2020-07-04 10:10:34 +0200

    showbar = 0

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=16", "-misc-font awesome 5 brands regular-medium-r-normal--17-120-100-100-p-0-iso10646-1", "-misc-font awesome 5 free regular-medium-r-normal--17-120-100-100-p-0-iso10646-1", "-misc-font awesome 5 free solid-black-r-normal--17-120-100-100-p-0-iso10646-1"};
 static const char dmenufont[]       = "monospace:size=10";
