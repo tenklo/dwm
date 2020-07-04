7d086c6ea2116de1f3d23befe9a0f498ee6d8082
tenklo <tenklo@pm.me>
2020-07-04 09:39:23 +0200

    Revert "added inplacerotate patch" dont like it, will try movestack
    This reverts commit d6fd728974a53964672a94cecad935980af16c45.

--- config.h
+++ config.h
@@ -84,8 +84,6 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,      inplacerotate,  {.i = +1} },
-	{ MODKEY|ShiftMask,             XK_k,      inplacerotate,  {.i = -1} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
